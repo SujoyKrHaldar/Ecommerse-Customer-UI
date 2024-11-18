1b3d2299168c561e8bf8195170d318110bb84d0d
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-11-19 00:32:40 +0530

    Setup custom notification ui

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,11 @@
-// import Notification from "./Notification";
+import Notification from "./Notification";
 import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <header className="z-50 fixed inset-0 w-full h-fit py-5">
-      {/* <Notification /> */}
+    <header className="z-50 fixed inset-0 w-full h-screen pointer-events-none py-5 flex flex-col justify-between">
       <Navbar />
+      <Notification />
     </header>
   );
 }
--- src/components/layout/Notification.jsx
+++ src/components/layout/Notification.jsx
@@ -1,5 +1,22 @@
+import { CircleCheckBig, X } from "lucide-react";
+
 function Notification() {
-  return <div>Notification</div>;
+  return (
+    <section className="container pointer-events-auto opacity-0">
+      <div
+        className={`flex items-center justify-between gap-8 p-4 border border-black bg-red-700`}
+      >
+        <div className="flex items-center gap-2">
+          <CircleCheckBig size={20} color="white" />
+          <p className="text-sm text-white">Something went wrong</p>
+        </div>
+
+        <div className="cursor-pointer">
+          <X color="white" size={20} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Notification;
